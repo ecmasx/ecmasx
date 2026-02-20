6e13106b2f103345ed6f6de758affb9a97d9d4d0
Maxim Madan <maximmadan3@gmail.com>
2026-02-21 01:28:07 +0200

    Add fonts and typography preview

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,14 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<link
+		rel="preload"
+		href="/fonts/inter/inter-variable.ttf"
+		as="font"
+		type="font/ttf"
+		crossorigin="anonymous"
+	/>
+</svelte:head>
 {@render children()}
